1ab7fc8042750f57a26c079426bfe599a14869cb
theGuardianCode <nickrichard878@gmail.com>
2025-01-20 19:06:31 +1000

    Added a plain map page for use in the android app

---
+++ mapView/mapView.js
@@ -0,0 +1,41 @@
+const map = L.map('map').setView([-27.4729, 153.0265], 15)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+function plotRacks(racks) {
+    racks.forEach(element => {
+        const mapsURL = `https://www.google.com/maps/search/?api=1&query=${element.latitude}%2C${element.longitude}`
+
+        let popupText = `
+            Address: ${element.address}<br>
+            Type: ${element.rack_type}<br>
+            Capacity: ${element.capacity}<br>
+            <a href="${mapsURL}">Open Directions</a>
+        `
+        L.marker([element.latitude, element.longitude]).addTo(map)
+            .bindPopup(popupText)
+    });
+}
+
+
+window.onload = async () => {
+    let offset = 0
+    let limit = 100
+    let racks = []
+
+    while (limit > 0) {
+        const url = `https://data.brisbane.qld.gov.au/api/explore/v2.1/catalog/datasets/bicycle-racks/records?limit=${limit}&offset=${offset}`
+
+        const res = await fetch(url)
+        const data = await res.json()
+
+        racks = racks.concat(data.results)
+
+        offset += limit
+        limit = data.total_count - offset
+    }
+    
+    plotRacks(racks)
+}
